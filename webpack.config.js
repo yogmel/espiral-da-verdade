5e79f9192cd187efb11a3d55879e535669dad50f
Mellina Yonashiro <yonashiro.mellina@gmail.com>
2019-03-19 14:06:23 -0300

    Revisão 19-03-2019

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,18 @@ module.exports = {
         'css-loader',
         'sass-loader'
       ]
-    }]
+    },
+    {
+      test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+      use: [{
+          loader: 'file-loader',
+          options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+          }
+      }]
+    }
+  ]
   },
   devtool: 'cheap-module-eval-source-map',
   devServer: {
